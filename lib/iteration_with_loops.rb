139430e71fdb9253334dd16a79abab2b86910162
Doug K <doug.kerzner@gmail.com>
2021-01-19 14:19:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ while row_index < src.count do
   element_index = 0 
   min_val = 0 
   while element_index < src[row_index].count do
-    if src[row_index][element_index] > min_val
+    if src[row_index][element_index] < min_val
       min_val = src[row_index][element_index]
     end 
     element_index += 1 
