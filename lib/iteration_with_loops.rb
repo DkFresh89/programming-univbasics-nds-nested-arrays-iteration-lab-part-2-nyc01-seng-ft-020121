b762d7f8793cb3e8cba1309a480f0bcc33e34de7
Doug K <doug.kerzner@gmail.com>
2021-01-19 14:11:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,10 +5,9 @@ evens = []
 row_index = 0 
 while row_index < src.count do
   element_index = 0 
-  min_val = 
   while element_index < src[row_index].count do
     if src[row_index][element_index] > min_val
-      min_val = src[row_index][element_index]
+      min_val = src[row_index][element_index].min
     end 
     element_index += 1 
   end 
